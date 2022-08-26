c4ed2d7fed957e2d09e5da5ff85543d04238552b
Utkan <utkanocal@gmail.com>
2022-08-26 11:45:49 +0300

    api endpoints

--- src/apiRequests.js
+++ src/apiRequests.js
@@ -1,10 +1,10 @@
 const key = "0e9d3030995b7d674944d3f5386f39f7"
 
  const requests = {
-    popularMovies : `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    popularMovies : `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
     topRatedMovies : `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    trendingMovies : `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-    upComingMovies : `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    trendingMovies : `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=3`,
+    upComingMovies : `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=2`,
 
 }
 
