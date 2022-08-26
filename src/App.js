b0e7458412e4d69fccd09d234770d40e854bbafb
Utkan <utkanocal@gmail.com>
2022-08-26 11:51:43 +0300

    new routes added -auth p.-

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main/Main";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Account from "./Pages/Account";
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </>
   );
